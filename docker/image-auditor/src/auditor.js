828fa645f4e3e72eaa6b1672e4f1008211d3a172
Chris <chris.peretti@gmail.com>
2016-05-04 10:51:45 +0200

    tcp

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,9 +1,34 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-const net = require('net');
-
+var net = require('net');
 var HOST = '172.17.0.1';
 var PORT = 2205;
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+
+        console.log('DATA ' + sock.remoteAddress + ':');
+        console.log(data);
+        console.log();
+
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
 var instruments = {"ti-ta-ti":"piano","pouet":"trumpet","trulu":"flute","gzi-gzi":"violin","boum-boum":"drum"};
 var activeMusicians = {};
@@ -54,29 +79,5 @@ setInterval(function (){
 }, 1000);
 
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
 
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-
-    // Add a 'data' event handler to this instance of socket
-    sock.on('data', function(data) {
-
-        console.log('DATA ' + sock.remoteAddress + ':');
-        console.log(data);
-        console.log();
-
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
-    });
-
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
-
-}).listen(PORT, HOST);
 
