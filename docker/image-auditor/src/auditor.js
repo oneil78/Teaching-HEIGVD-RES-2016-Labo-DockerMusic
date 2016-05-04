0f216ab1dc7742c3dabcd37a60bff7e074b08e56
Chris <chris.peretti@gmail.com>
2016-05-04 12:35:10 +0200

    tcp

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -10,30 +10,19 @@ net.createServer(function(sock) {
 
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+	console.log('Active musician sent');
+	console.log();
 
-    // Add a 'data' event handler to this instance of socket
-    sock.on('data', function(data) {
-
-        console.log('TCP connection : active musician sent');
-        console.log();
-
-		var tabMusicians = [];
-		for (var key in activeMusicians) {
-			var tabMusician = {};
-			tabMusician["uuid"] = activeMusicians[key].uuid;
-			tabMusician["instrument"] = activeMusicians[key].instrument;
-			tabMusician["activeSince"] = activeMusicians[key].activeSince;
-			tabMusicians.push(tabMusician);
-		}
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write(JSON.stringify(tabMusicians) + '\n');
-		sock.close();
-    });
-
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
+	var tabMusicians = [];
+	for (var key in activeMusicians) {
+		var tabMusician = {};
+		tabMusician["uuid"] = activeMusicians[key].uuid;
+		tabMusician["instrument"] = activeMusicians[key].instrument;
+		tabMusician["activeSince"] = activeMusicians[key].activeSince;
+		tabMusicians.push(tabMusician);
+	}
+	// Write the data back to the socket, the client will receive it as data from the server
+	sock.write(JSON.stringify(tabMusicians) + '\n');
 
 }).listen(PORT, HOST);
 
